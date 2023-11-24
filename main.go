a68141940468019aa2aae0ee1fce527358cc70d8
Tyler Smith <tylerlwsmith@gmail.com>
2023-11-23 22:40:57 -0800

    Add site index endpoint

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>Hello, world!</h1>")
+	})
+
+	http.ListenAndServe(":3000", r)
 }
