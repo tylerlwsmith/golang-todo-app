55fcc52486b57712ec51d29ed499dc1c543cce80
Tyler Smith <tylerlwsmith@gmail.com>
2023-11-24 18:43:40 -0800

    Add comment about log.Printf vs fmt.Printf

--- main.go
+++ main.go
@@ -26,6 +26,9 @@ func main() {
 	done := make(chan bool)
 
 	go http.ListenAndServe(":"+p, r)
+
+	// Not using fmt.Printf because log.Printf works better with threads.
+	// https://stackoverflow.com/a/41390023/7759523
 	log.Printf("Now listening at http://localhost:%v", p)
 
 	<-done
