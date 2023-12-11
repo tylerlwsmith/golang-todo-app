e42fb5d53874dd2c7b7508d0dceb2db0551f3835
Tyler Smith <tylerlwsmith@gmail.com>
2023-12-10 21:29:54 -0800

    Refactor init function out of repositories.go

--- repositories/repositories.go
+++ repositories/repositories.go
@@ -2,11 +2,7 @@ package repositories
 
 import "golang-todo-app/models"
 
-var todos []models.Todo
-
-func init() {
-	todos = append(todos, models.Todo{Id: 1, Description: "Fake DB", Done: false})
-}
+var todos = []models.Todo{{Id: 1, Description: "Fake DB", Done: false}}
 
 func GetTodos() []models.Todo {
 	return todos
