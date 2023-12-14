004be7f3c13d6ab55564cca6f2c01673f905866f
Tyler Smith <tylerlwsmith@gmail.com>
2023-12-13 22:32:04 -0800

    Uppercase form _method field in middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,13 +1,16 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func UseFormMethod(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			m := r.PostFormValue("_method")
 			if m != "" {
-				r.Method = m
+				r.Method = strings.ToUpper(m)
 			}
 		}
 
