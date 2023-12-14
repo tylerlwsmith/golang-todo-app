070d183d29189fba1deb47b5c941513617ccf0f3
Tyler Smith <tylerlwsmith@gmail.com>
2023-12-13 22:39:10 -0800

    Limit http verbs for form method overrides

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -9,7 +9,7 @@ func UseFormMethod(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			m := r.PostFormValue("_method")
-			if m != "" {
+			if m == "DELETE" || m == "PATCH" || m == "PUT" {
 				r.Method = strings.ToUpper(m)
 			}
 		}
