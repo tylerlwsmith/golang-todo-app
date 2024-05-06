9aa303e1539d4a245bdd24d5484d6d5f86fb49f3
Tyler Smith <tylerlwsmith@gmail.com>
2024-05-05 20:31:33 -0700

    Fix 'listening' message, graceful shutdown, update handler

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -18,6 +24,7 @@ func main() {
 	r.HandleFunc("/tasks", handlers.Index).Methods("GET")
 	r.HandleFunc("/tasks", handlers.Store).Methods("POST")
 	r.HandleFunc("/tasks/{id}", handlers.Edit).Methods("GET")
+	r.HandleFunc("/tasks/{id}", handlers.Update).Methods("PUT")
 	r.HandleFunc("/tasks/{id}", handlers.Delete).Methods("DELETE")
 
 	p := os.Getenv("PORT")
@@ -25,15 +32,54 @@ func main() {
 		p = "3000"
 	}
 
-	// Allow app to print listening message after starting the server.
-	// https://stackoverflow.com/a/48250354/7759523
-	done := make(chan bool)
+	// https://github.com/gorilla/mux?tab=readme-ov-file#graceful-shutdown
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 
-	go http.ListenAndServe(":"+p, middleware.UseFormMethod(r))
+	// Decouple listen and serve so I can display message _after_ listening
+	// on an avialable port.
+	// https://stackoverflow.com/a/53332769/7759523
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
 
-	// Not using fmt.Printf because log.Printf works better with threads.
-	// https://stackoverflow.com/a/41390023/7759523
-	log.Printf("Now listening at http://localhost:%v", p)
+	// https://github.com/gorilla/mux?tab=readme-ov-file#graceful-shutdown
+	s := &http.Server{
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      middleware.UseFormMethod(r), // Pass our instance of gorilla/mux in.
+	}
 
-	<-done
+	fmt.Printf("Listening on port %v\n", p)
+
+	go func() {
+		if err := s.Serve(l); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	s.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
 }
